3ec627260ac05eb34d41fb4d947343c148f0a2b8
Geovani Amaral <geovani.af4@gmail.com>
2022-02-13 20:10:18 -0300

    quiz project

---
+++ quizz/lib/main.dart
@@ -0,0 +1,149 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:quizz/model/quiz.dart';
+
+import 'components/answer.dart';
+import 'components/error.dart';
+import 'network/result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.white),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Quiz quiz;
+  late List<Results> results;
+  late Color c;
+  Random random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> fetchQuestions() async {
+    var res = await http.get("https://opentdb.com/api.php?amount=20");
+    var decRes = jsonDecode(res.body);
+    c = Colors.black;
+    quiz = Quiz.fromJson(decRes);
+    results = quiz.results;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                "Quiz App",
+              ),
+              SizedBox(width: 8,),
+              Icon(Icons.quiz, color: Colors.white,)
+            ],
+          ),
+
+        elevation: 1,
+      ),
+      body: RefreshIndicator(
+        onRefresh: fetchQuestions,
+        child: FutureBuilder(
+            future: fetchQuestions(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return const Text('Press button to start.');
+                case ConnectionState.active:
+                case ConnectionState.waiting:
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case ConnectionState.done:
+                  if (snapshot.hasError) return errorData(snapshot);
+                  return questionList();
+              }
+            }),
+      ),
+    );
+  }
+
+  ListView questionList() {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      itemCount: results.length,
+      itemBuilder: (context, index) => Card(
+        color: Colors.white,
+        elevation: 1,
+        child: QuestionItem(index),
+      ),
+    );
+  }
+
+  ExpansionTile QuestionItem(int index) {
+    return ExpansionTile(
+        title: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                results[index].question!,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              FittedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FilterChip(
+                      backgroundColor: Colors.grey[100],
+                      label: Text(results[index].category!),
+                      onSelected: (b) {},
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    FilterChip(
+                      backgroundColor: Colors.grey[100],
+                      label: Text(
+                        results[index].difficulty!,
+                      ),
+                      onSelected: (b) {},
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        leading: CircleAvatar(
+          backgroundColor: Colors.grey[100],
+          child: Text(index.toString()),
+        ),
+        children: results[index].allAnswers!.map((m) {
+          return AnswerWidget(results, index, m);
+        }).toList(),
+      );
+  }
+}
